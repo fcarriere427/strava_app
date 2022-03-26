5320718461a38d740946d324b3f5159a70a1e2d9
florian <florian.carriere@gmail.com>
2022-03-26 23:13:34 +0100

    add class

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import logo from './atom.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p><a href="strava_old_app">Ancienne appli Strava</a>
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <div className="tracker">
+          <StravaTracker name = "Florian"/>
+        </div>
+        <p><a href="strava_old_app">Ancienne appli Strava</a></p>
+      </div>
+    );
+  }
+}
+
+class StravaTracker extends React.Component {
+  render() {
+      return (
+        <div className="tracker">
+          <h1>Tracker for {this.props.name}</h1>
+          <ul>
+            <li>Instagram</li>
+            <li>WhatsApp</li>
+            <li>Oculus</li>
+          </ul>
+        </div>
+      );
+    }
 }
 
 export default App;
