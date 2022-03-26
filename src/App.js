fc3082be9c023d6179b75a1c42b41a1517cd2fc9
florian <florian.carriere@gmail.com>
2022-03-26 12:06:29 +0100

    clean home

--- src/App.js
+++ src/App.js
@@ -6,24 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React puis voir Venise et mourir
-        </a>
-
-        <p> Lien vers
-          <a href="strava_old_app">
-            l'ancienne appli Strava
-          </a>
-        </p>
-
+        <p> Lien vers <a href="strava_old_app">l'ancienne appli Strava</a></p>
       </header>
     </div>
   );
