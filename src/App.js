a2f622b092a6bf78d536373a527c2bdcad8ebc43
florian <florian.carriere@gmail.com>
2022-03-26 22:25:15 +0100

    change logo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import logo from './favicon.ico';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        // <link rel="icon" type="image/png" href="./img/favicon.ico"/>
+        <p><a href="strava_old_app">Ancienne appli Strava</a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
