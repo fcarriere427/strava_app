54d15dc400783a13cc8531af32a52a870a5fd77f
florian <florian.carriere@gmail.com>
2022-03-27 22:29:54 +0200

    remove logo

--- src/components/Global/Header/Header.js
+++ src/components/Global/Header/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          // <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to my personal Strava App</h1>
         </header>
       </div>
