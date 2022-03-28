41f37f36e0aa8aa8f9131b8f63861c341fec2533
florian <florian.carriere@gmail.com>
2022-03-28 22:00:27 +0200

    correct

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import './App.css';
 import Header from './components/Global/Header/Header';
 import Footer from './components/Global/Footer/Footer';
-import Tracker from './components/Global/Tracker';
+import Tracker from './components/Tracker/Tracker';
 
 
 class App extends React.Component {
