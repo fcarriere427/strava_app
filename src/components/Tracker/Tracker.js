d66d5ad29256cdec95657f9337cc0ad4de30a488
florian <florian.carriere@gmail.com>
2022-04-02 23:43:27 +0200

    add let...

--- src/components/Tracker/Tracker.js
+++ src/components/Tracker/Tracker.js
@@ -12,7 +12,7 @@ class Tracker extends Component {
   componentDidMount(){
 
     // Récupération de la date de la dernière activité (format lisible, en local time)
-    url = 'https://letsq.xyz/strava/last_activity';
+    let url = 'https://letsq.xyz/strava/last_activity';
     axios.get(url)
     .then(
       (response) => {
