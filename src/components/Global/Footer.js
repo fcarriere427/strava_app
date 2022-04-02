e9a370d06805a911bf532885627ea3ceb78d8a4e
florian <florian.carriere@gmail.com>
2022-04-03 00:25:51 +0200

    add date to footer

--- src/components/Global/Footer.js
+++ src/components/Global/Footer.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 class Footer extends Component {
+
+  constructor(props) {
+    super(props);
+    let today = new Date();
+    let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    this.state = { date: date };
+   }
+
   render() {
     return (
         <footer className="App-footer">
           <p><a href="strava_old_app">Ancienne appli Strava</a></p>
+          <p>Current date : {this.state.date} </p>
         </footer>
     );
   }
