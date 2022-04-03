ab158f9b8ee32bfcc9137517ac0db69fe476cd30
florian <florian.carriere@gmail.com>
2022-04-03 18:04:25 +0200

    test

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -24,16 +24,16 @@ class Tracker extends Component {
     )
 
     // Récupération du cumul de l'année
-    let year = '2022';
+    let today = new Date();
+    let year = today.getFullYear();
     url = 'https://letsq.xyz/strava/year_distances';
     axios.get(url)
     .then(
       (response) => {
-        console.log("response.data = " + response.data);
-        let distance = response.data.year;
-        console.log("response = " + response);
         console.log("year = " + year);
-        console.log("response.year = " + response.year);
+        console.log("response.data = " + response.data);
+        let distance = response.data.`${year}`;
+        console.log("distance  = " + response.data.`${year}`);
         //this.setState({ yearDistance: response.data.year_distance });
         this.setState({ yearDistance: 300 });
       },
