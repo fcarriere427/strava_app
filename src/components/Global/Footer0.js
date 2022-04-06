c877ab00ee6599846de80c8503c2c0282ee6fc11
florian <florian.carriere@gmail.com>
2022-04-06 21:37:35 +0200

    simplify footer

--- src/components/Global/Footer0.js
+++ src/components/Global/Footer0.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { today: "" };
+  }
+
   componentDidMount(){
+    let today = new Date();
+    let current = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
     this.setState({ today: current });
   }
 
   render() {
-    let today = new Date();
-    let current = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
-
     return (
         <footer className="App-footer">
           <p><a href="strava_old_app">Ancienne appli Strava</a></p>
-          <p>Current date : {current} </p>
+          <p>Current date : {this.state.today} </p>
         </footer>
     );
   }
