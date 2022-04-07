87e8761d5edb20b22bc17ca51e2dde223bae2612
florian <florian.carriere@gmail.com>
2022-04-07 21:55:46 +0200

    test

--- src/components/GaugeChart.js
+++ src/components/GaugeChart.js
@@ -9,7 +9,7 @@ let gaugeData = [
 class GaugeChart extends Component {
   constructor(props) {
     super(props);
-    console.log("props = " + this.props.value);
+    console.log("props = " + props.value);
   }
 
   componentDidMount() {
