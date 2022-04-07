2df50e019325ac753bf031fb3a68d87f920c1f55
florian <florian.carriere@gmail.com>
2022-04-07 22:56:20 +0200

    design

--- src/components/GaugeChart.js
+++ src/components/GaugeChart.js
@@ -12,20 +12,18 @@ class GaugeChart extends Component {
   }
 
   render() {
-    const chartStyle = {
-        height: 250,
-    };
     let needle = 0.5 + (this.props.delta/100) ; // si delta = 0 --> aiguille à 50% -->
     return (
       <div className="Tracker">
         <Chart
           id="tracker-gauge"
-          style={chartStyle}
-          nrOfLevels={30}
-          colors={['#5BE12C', '#F5CD19', '#EA4228']}
-          arcWidth={0.3}
+          style={width:'100%'}
+          nrOfLevels={3}
+          colors={['#EA4228', '#5BE12C', '#EA4228']}
+          arcWidth={0.5}
+          hideText={true}
           percent={needle}
-          formatTextValue={value => value + ' days'}
+          // formatTextValue={value => value + ' days'}
         />
       </div>
     )
