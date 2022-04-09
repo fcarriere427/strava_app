ee31843fe8aaad3b1e07e46fea3cb19583ea7e8b
florian <florian.carriere@gmail.com>
2022-04-09 16:23:50 +0200

    test

--- src/components/Tracker/Tracker.js
+++ src/components/Tracker/Tracker.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-//import { Container } from 'bootstrap'
+//import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'reactstrap'
 import GaugeChart from './Gauge'
 import daysInYear from '../../utils/functions'
 
