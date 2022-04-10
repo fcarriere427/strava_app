78a4c234b94a65c24884b503706f2588341a557f
florian <florian.carriere@gmail.com>
2022-04-10 17:15:01 +0200

    correct

--- src/components/Tracker/Target.js
+++ src/components/Tracker/Target.js
@@ -5,7 +5,7 @@ class Target extends Component {
   render(){
     return(
       <div>
-        <input type="range" min="500" max ="1500" value={this.props.target} onChange={evt => this.props.handler}/>
+        <input type="range" min="500" max ="1500" value={this.props.value} onChange={evt => this.props.handler}/>
         <p> Target: {this.props.value} </p>
       </div>
     )
