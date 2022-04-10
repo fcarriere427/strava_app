59fd610f24dac31523dafc632b18d4632da431a2
florian <florian.carriere@gmail.com>
2022-04-10 17:13:53 +0200

    add div

--- src/components/Tracker/Target.js
+++ src/components/Tracker/Target.js
@@ -4,8 +4,10 @@ import React, { Component } from 'react'
 class Target extends Component {
   render(){
     return(
-      <input type="range" min="500" max ="1500" value={this.props.target} onChange={evt => this.props.handler}/>
-      <p> Target: {this.props.value} </p>
+      <div>
+        <input type="range" min="500" max ="1500" value={this.props.target} onChange={evt => this.props.handler}/>
+        <p> Target: {this.props.value} </p>
+      </div>
     )
   }
 }
