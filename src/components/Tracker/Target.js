43e61a7461a09f10ff82e3078958388eea9a34f3
florian <florian.carriere@gmail.com>
2022-04-11 21:44:57 +0200

    test

--- src/components/Tracker/Target.js
+++ src/components/Tracker/Target.js
@@ -6,36 +6,30 @@ class Target extends Component {
     super(props);
     this.state = {value: ''};
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
     this.setState({value: event.target.value});
   }
 
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
-
   render(){
 
-    return (
-    <form onSubmit={this.handleSubmit}>
-      <label>
-        Name:
-        <input type="text" value={this.state.value} onChange={this.handleChange} />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
-   );
+   //  return (
+   //  <form onSubmit={this.handleSubmit}>
+   //    <label>
+   //      Name:
+   //      <input type="text" value={this.state.value} onChange={this.handleChange} />
+   //    </label>
+   //    <input type="submit" value="Submit" />
+   //  </form>
+   // );
 
-    // return(
-    //   <div>
-    //     <input type="range" min="500" max ="1500" value={this.props.value} onChange={evt => this.props.handler}/>
-    //     <p> Target: {this.props.value} </p>
-    //   </div>
-    // )
+    return(
+      <div>
+        <input type="range" min="500" max ="1500" value={this.state.value} onChange={this.handleChange}/>
+        <p> Target: {this.state.value} </p>
+      </div>
+    )
 
   }
 }
