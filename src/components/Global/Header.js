42ab00b40cd9b119f7020718c8504992c120b912
florian <florian.carriere@gmail.com>
2022-04-11 22:52:35 +0200

    header padding 3

--- src/components/Global/Header.js
+++ src/components/Global/Header.js
@@ -6,11 +6,11 @@ import { Container } from 'reactstrap'
 class Header extends Component {
   render() {
     return (
-      <Container fluid className = "bg-warning text-black text-center my-auto">
+      <Container fluid className = "bg-warning text-black text-center my-auto ">
         <div>
           <header>
             {/* <img src={logo} className="App-logo" alt="logo" /> */}
-            <p className="fw-bold">{this.props.name}'s Strava App</p>
+            <p className="fw-bold py-3">{this.props.name}'s Strava App</p>
           </header>
         </div>
       </Container>
