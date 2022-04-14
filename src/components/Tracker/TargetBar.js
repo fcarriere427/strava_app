df1d9db7ff5671ed5fe29fc0f0ff334ca3fa6959
florian <florian.carriere@gmail.com>
2022-04-14 22:45:52 +0200

    import

--- src/components/Tracker/TargetBar.js
+++ src/components/Tracker/TargetBar.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Row, Col } from 'reactstrap'
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 class TargetBar extends Component {
