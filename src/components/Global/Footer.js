c0191e8f7fe72f0dd212cc882b1cdc9bc68e2982
florian <florian.carriere@gmail.com>
2022-04-15 00:13:33 +0200

    finish

--- src/components/Global/Footer.js
+++ src/components/Global/Footer.js
@@ -11,9 +11,9 @@ class Footer extends Component {
       <Container fluid className = "bg-dark text-white text-center">
         <footer>
           <p>Current date : {date_str} </p>
-          <p className="fw-light"><a href="strava_old_app">Ancienne appli Strava</a></p>
         </footer>
       </Container>
+      <p className="fw-light text-black"><a href="strava_old_app">Ancienne appli Strava</a></p>
     );
   }
 }
