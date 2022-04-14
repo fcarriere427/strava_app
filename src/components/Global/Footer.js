f15fca522e3a1f6d98ba93c768513b194499cee6
florian <florian.carriere@gmail.com>
2022-04-15 00:14:59 +0200

    test

--- src/components/Global/Footer.js
+++ src/components/Global/Footer.js
@@ -12,10 +12,10 @@ class Footer extends Component {
         <footer>
           <p>Current date : {date_str} </p>
         </footer>
+        <div>
+          <p className="fw-light text-black"><a href="strava_old_app">Ancienne appli Strava</a></p>
+        </div>
       </Container>
-      <div>
-        <p className="fw-light text-black"><a href="strava_old_app">Ancienne appli Strava</a></p>
-      </div>
     );
   }
 }
