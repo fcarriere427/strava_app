aa855c0978292e3bcdfd6d569abeadd20ce61dd2
florian <florian.carriere@gmail.com>
2022-04-15 23:06:02 +0200

    add navbar

--- src/components/Global/Header.js
+++ src/components/Global/Header.js
@@ -1,21 +1,76 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap'
+import { Container, Row, Col, Nav, Navbar, NavbarToggler, Collapse, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText, NavbarBrand  } from 'reactstrap'
 
 // import logo from './logo.png';
 
 class Header extends Component {
   render() {
     return (
-      <Container fluid className = "bg-primary text-white text-center my-auto ">
-        <div>
-          <header>
-            {/* <img src={logo} className="App-logo" alt="logo" /> */}
-            <p className="fw-bold py-3">{this.props.name}'s Strava App</p>
-          </header>
-        </div>
-      </Container>
+      <div>
+        <Navbar
+          color="light"
+          expand="md"
+        >
+          <NavbarBrand href="/">
+            reactstrap
+          </NavbarBrand>
+          <NavbarToggler onClick={function noRefCheck(){}} />
+          <Collapse navbar>
+            <Nav
+              className="me-auto"
+              navbar
+            >
+              <NavItem>
+                <NavLink href="/components/">
+                  Components
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+              <UncontrolledDropdown
+                inNavbar
+                nav
+              >
+                <DropdownToggle
+                  caret
+                  nav
+                >
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+            <NavbarText>
+              Simple Text
+            </NavbarText>
+          </Collapse>
+        </Navbar>
+      </div>
     );
   }
 }
+
+// <Container fluid className = "bg-primary text-white text-center my-auto ">
+//   <div>
+//     <header>
+//       {/* <img src={logo} className="App-logo" alt="logo" /> */}
+//       <p className="fw-bold py-3">{this.props.name}'s Strava App</p>
+//     </header>
+//   </div>
+// </Container>
 
 export default Header;
--- src/components/Tracker/TargetBlock.js
+++ src/components/Tracker/TargetBlock.js
@@ -1,85 +1,28 @@
 import React, { Component } from 'react'
-import { Container, Row, Col, Nav, Navbar, NavbarToggler, Collapse, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText, NavbarBrand } from 'reactstrap'
+import { Container, Row, Col } from 'reactstrap'
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 class TargetBlock extends Component {
   render(){
     return(
-      <div>
-        <Navbar
-          color="light"
-          expand="md"
-        >
-          <NavbarBrand href="/">
-            reactstrap
-          </NavbarBrand>
-          <NavbarToggler onClick={function noRefCheck(){}} />
-          <Collapse navbar>
-            <Nav
-              className="me-auto"
-              navbar
-            >
-              <NavItem>
-                <NavLink href="/components/">
-                  Components
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">
-                  GitHub
-                </NavLink>
-              </NavItem>
-              <UncontrolledDropdown
-                inNavbar
-                nav
-              >
-                <DropdownToggle
-                  caret
-                  nav
-                >
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-            <NavbarText>
-              Simple Text
-            </NavbarText>
-          </Collapse>
-        </Navbar>
-      </div>
-
-        /*
-            <Container fluid className='text-black text-center'>
-            <Row className="py-2">
-            <div>
+      <Container fluid className='text-black text-center'>
+        <Row className="py-2">
+          <div>
             <input type="range" min="500" max ="1500" step="10" value={this.props.value} onChange={this.props.updateHandler}/>
             <p> Target: {this.props.value} </p>
-            </div>
-            </Row>
-            <Row className="py-2">
-            <div>
+          </div>
+        </Row>
+        <Row className="py-2">
+          <div>
             <input type="button" value="reset" onClick={this.props.resetHandler}/>
-            </div>
-            </Row>
-        </Container> */
-
-      )
-    }
+          </div>
+        </Row>
+      </Container>
+    )
   }
+}
 
-        export {
-          TargetBlock
-        }
+export {
+  TargetBlock
+}
