e8e4c34b0ec70b6342d9f01b9aee023da100542f
florian <florian.carriere@gmail.com>
2022-04-16 15:29:59 +0200

    add compnents index

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from "./Header";
+export { default as Footer } from "./Footer";
+export { default as Tracker } from "./Tracker";
+export { default as List } from "./List";
