cdf2024257056a8829bc8367aab2912d062df9ce
florian <florian.carriere@gmail.com>
2022-04-16 08:47:56 +0200

    update header

--- src/components/Global/Header.js
+++ src/components/Global/Header.js
@@ -42,7 +42,7 @@ class Header extends Component {
               </NavItem>
             </Nav>
             <NavbarText>
-              Welcome <br/> {this.props.name}!
+              {this.props.name}
             </NavbarText>
           </Collapse>
         </Navbar>
