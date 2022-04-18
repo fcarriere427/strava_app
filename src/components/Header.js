94c9b3c0a9fe711f12559ac558e48fb9650c47b5
florian <florian.carriere@gmail.com>
2022-04-18 18:10:09 +0200

    test ??

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,7 +37,7 @@ class Header extends Component {
             <NavItem>
               <NavLink
                 href="/list"
-                active="true"
+                active=true
               >
                 List
               </NavLink>
