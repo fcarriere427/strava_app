7ab1218933efa5cefd208a4428fe466a0dba7632
florian <florian.carriere@gmail.com>
2022-04-18 18:07:43 +0200

    test list

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,7 +37,7 @@ class Header extends Component {
             <NavItem>
               <NavLink
                 href="/list"
-                active={window.location === '/list'}
+                activeClassName={window.location === '/list'}
               >
                 List
               </NavLink>
