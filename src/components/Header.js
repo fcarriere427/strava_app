a23f82fb1755eca73ac30b22a616d3c01ac9f13c
florian <florian.carriere@gmail.com>
2022-04-18 18:08:58 +0200

    typo

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,7 +37,7 @@ class Header extends Component {
             <NavItem>
               <NavLink
                 href="/list"
-                activeClassName=true
+                activeClassName="true"
               >
                 List
               </NavLink>
