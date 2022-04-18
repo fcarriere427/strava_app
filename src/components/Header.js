fe53ec5d8548ffd2a18216e112599772fd2691d9
florian <florian.carriere@gmail.com>
2022-04-18 18:58:03 +0200

    back ?

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,71 +4,66 @@ import Tracker from './Tracker'
 import List from './List'
 
 
-// import logo from './logo.png';
-
 class Header extends Component {
-  render() {
-    // console.log("window.location = " + window.location)
-    return (
-      <div className="sticky-top">
-        <Navbar
-          color="warning"
-          expand="xs"
-        >
-          <NavbarBrand href="/">
-            <img className="Image-fluid" src={require("../assets/logo.png")}
-              alt="logo"
-              width="40"
-              height="40"
-            >
-            </img>
-          </NavbarBrand>
-          <Nav
-            className="me-auto"
-            navbar
+
+render() {
+  return (
+    <div className="sticky-top">
+
+      <Navbar bg="warning" expand="xs">
+
+        <NavbarBrand href="/">
+          <img className="Image-fluid" src={require("../assets/logo.png")}
+            alt="logo"
+            width="40"
+            height="40"
           >
-            <NavItem>
-              <NavLink
-                to ="/tracker"
-                // style={ isActive => ({ color: isActive ? 'green' : 'blue' })}
-                style={{color: "green"}}
-              >
-                Tracker
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                to ="/list"
-                // style={({ isActive }) => ({ color: isActive ? 'green' : 'blue' })}
-              >
-                List
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                href ="/reports"
-                // style={({ isActive }) => ({ color: isActive ? 'green' : 'blue' })}
-              >
-                Reports
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                href="/strava_old_app"
-                // className="nav-link"
-              >
-                Old app
-              </NavLink>
-            </NavItem>
-          </Nav>
+          </img>
+        </NavbarBrand>
+
+        <Nav activeKey={location.pathname}>
+          <NavItem>
+            <NavLink
+              href ="/tracker"
+              // style={ isActive => ({ color: isActive ? 'green' : 'blue' })}
+              style={{color: "green"}}
+            >
+              Tracker
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              href ="/list"
+              // style={({ isActive }) => ({ color: isActive ? 'green' : 'blue' })}
+            >
+              List
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              href ="/reports"
+              // style={({ isActive }) => ({ color: isActive ? 'green' : 'blue' })}
+            >
+              Reports
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              href="/strava_old_app"
+              // className="nav-link"
+            >
+              Old app
+            </NavLink>
+          </NavItem>
           <NavbarText>
             {this.props.name}
           </NavbarText>
 
-        </Navbar>
-      </div>
-    );
-  }
+        </Nav>
+
+      </Navbar>
+    </div>
+  );
 }
 
 export default Header;
