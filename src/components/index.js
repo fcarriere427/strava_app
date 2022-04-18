4f46ce9a96951740ebe7748adb1fc0131df21a84
florian <florian.carriere@gmail.com>
2022-04-18 17:55:44 +0200

    add export indes

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as Tracker } from "./Tracker";
 export { default as List } from "./List";
+export { default as Reports } from "./Reports";
