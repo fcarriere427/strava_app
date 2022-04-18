ed6c9b2c1c347bb206fd55f0449864ec59cb95b9
florian <florian.carriere@gmail.com>
2022-04-18 17:52:47 +0200

    create Reports

---
+++ src/components/Reports.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Reports extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      activitiesList: []
+    };
+  }
+
+  render() {
+    return(
+      <div>
+        <p> To do :-)</p>
+      </div>
+    );
+  }
+}
+
+export default Reports;
