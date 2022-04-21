20978923b705a214fd1605a4269593188df7c229
florian <florian.carriere@gmail.com>
2022-04-21 21:53:59 +0200

    this.props...

--- src/components/List/ActivitySummary.js
+++ src/components/List/ActivitySummary.js
@@ -5,15 +5,15 @@ import { Row, Col } from 'reactstrap'
 class ActivitySummary extends Component {
   render(){
     return(
-      <Row key={index}>
+      <Row key={this.props.index}>
         <Col xs="2">
-          <p>{d.doc.id}</p>
+          <p>{this.props.data.doc.id}</p>
         </Col>
         <Col xs="5">
-          <p>{d.doc.start_date_local}</p>
+          <p>{this.props.data.doc.start_date_local}</p>
         </Col>
         <Col xs="5">
-          <p>{d.doc.distance}</p>
+          <p>{this.props.data.doc.distance}</p>
         </Col>
       </Row>
     )
