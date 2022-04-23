0973d4d87c1eafeaedce77f8f9eaee00aff0a500
florian <florian.carriere@gmail.com>
2022-04-23 17:39:40 +0200

    import useState

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Container, Row, Col } from 'reactstrap'
 import { useParams } from "react-router-dom"
 
