80c8dc4f1b0b62f948bf4b4f5acae41184cc8c0f
florian <florian.carriere@gmail.com>
2022-04-23 10:23:00 +0200

    complete activity display

--- src/components/List/ActivitySummary.js
+++ src/components/List/ActivitySummary.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Row, Col } from 'reactstrap'
+import { strTime, strSpeed } from './functions'
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 class ActivitySummary extends Component {
@@ -9,11 +10,20 @@ class ActivitySummary extends Component {
         <Col xs="2">
           <p>{this.props.data.doc.id}</p>
         </Col>
-        <Col xs="5">
-          <p>{this.props.data.doc.start_date_local}</p>
+        <Col xs="2">
+          <p>{this.props.data.doc.name}</p>
         </Col>
-        <Col xs="5">
-          <p>{this.props.data.doc.distance}</p>
+        <Col xs="2">
+          <p>{this.props.data.doc.start_date_local.substring(0,10)}</p>
+        </Col>
+        <Col xs="2">
+          <p>{strTime(this.props.data.doc}</p>
+        </Col>
+        <Col xs="2">
+          <p>{strSpeed(this.props.data.doc}</p>
+        </Col>
+        <Col xs="2">
+          <p>{Math.round(this.props.data.doc.distance / 1000 * 100) / 100}km</p>
         </Col>
       </Row>
     )
