9477526dd72b5d2aac40442250880b288eec8dc9
florian <florian.carriere@gmail.com>
2022-04-23 17:49:59 +0200

    correct

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -18,7 +18,7 @@ function Activity() {
 
 // Récupération des activités pour l'année donnée
 function getActivity(id) {
-  let url = 'https://letsq.xyz/api/strava/activities?id=' + id;
+  let url = 'https://letsq.xyz/api/strava/activity?id=' + id;
   axios.get(url)
   .then(
     (response) => { return response.data },
