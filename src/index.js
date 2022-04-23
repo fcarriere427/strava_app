a7c0a06e4d51f2dc2760715b308c31f62179356e
florian <florian.carriere@gmail.com>
2022-04-23 22:12:51 +0200

    remove bootstrap import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
