66a400509390a373cf72b74db5b95a85f53298b2
florian <florian.carriere@gmail.com>
2022-04-24 00:25:49 +0200

    test

--- src/components/Activity/Map.js
+++ src/components/Activity/Map.js
@@ -12,7 +12,7 @@ class Map extends Component {
         center={[51.505, -0.09]}
         zoom={13}
         scrollWheelZoom={false}
-        style={{height:"300px"}}
+        style={{width: "80%"; height: "80%"}}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
