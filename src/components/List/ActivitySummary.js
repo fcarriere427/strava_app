e73a50415bc37dcb16da5ac36ec846fe3a150f53
florian <florian.carriere@gmail.com>
2022-04-23 10:32:40 +0200

    style

--- src/components/List/ActivitySummary.js
+++ src/components/List/ActivitySummary.js
@@ -1,31 +1,35 @@
 import React, { Component } from 'react'
-import { Row, Col } from 'reactstrap'
+import { Container, Row, Col } from 'reactstrap'
 import { strTime, strSpeed } from './functions'
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 class ActivitySummary extends Component {
   render(){
     return(
-      <Row>
-        <Col xs="2">
-          <p>{this.props.data.doc.id}</p>
-        </Col>
-        <Col xs="2">
-          <p>{this.props.data.doc.name}</p>
-        </Col>
-        <Col xs="2">
-          <p>{this.props.data.doc.start_date_local.substring(0,10)}</p>
-        </Col>
-        <Col xs="2">
-          <p>{strTime(this.props.data.doc)}</p>
-        </Col>
-        <Col xs="2">
-          <p>{strSpeed(this.props.data.doc)}</p>
-        </Col>
-        <Col xs="2">
-          <p>{Math.round(this.props.data.doc.distance / 1000 * 100) / 100}km</p>
-        </Col>
-      </Row>
+      <Container className="bg-light text-black border py-2">
+        <Row>
+          <Col xs="4">
+            <p>{this.props.data.doc.start_date_local.substring(0,10)}</p>
+          </Col>
+          <Col xs="4">
+            <p>{Math.round(this.props.data.doc.distance / 1000 * 100) / 100}km</p>
+          </Col>
+          <Col xs="4">
+            <p>{strTime(this.props.data.doc)}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="4">
+            <p>{strSpeed(this.props.data.doc)}</p>
+          </Col>
+          <Col xs="4">
+            <p>{this.props.data.doc.id}</p>
+          </Col>
+          <Col xs="4">
+            <p>{this.props.data.doc.name}</p>
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
