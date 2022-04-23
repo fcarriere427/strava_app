605d2a5bf13a4f2ecb0f0d354d872a38972b256f
florian <florian.carriere@gmail.com>
2022-04-24 00:30:06 +0200

    test

--- src/components/Activity/Map.js
+++ src/components/Activity/Map.js
@@ -5,11 +5,11 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 class Map extends Component {
 
   render() {
-    const defaultPosition = [48.87, 2.24];
+    const position = [48.87, 2.24];
 
     return (
       <MapContainer
-        center={[51.505, -0.09]}
+        center={position}
         zoom={13}
         scrollWheelZoom={false}
         style={{height: '500px'}}
@@ -18,7 +18,7 @@ class Map extends Component {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={position}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
