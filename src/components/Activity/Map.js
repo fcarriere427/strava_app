3babc453b99495a6b3a5a54d9de8d0b5c52cd23f
florian <florian.carriere@gmail.com>
2022-04-24 00:23:55 +0200

    test

--- src/components/Activity/Map.js
+++ src/components/Activity/Map.js
@@ -8,7 +8,12 @@ class Map extends Component {
     const defaultPosition = [48.87, 2.24];
 
     return (
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{height:"300px"}}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
