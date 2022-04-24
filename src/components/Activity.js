1ab09e06b109c73094ba9079c91cf6541eb018d2
florian <florian.carriere@gmail.com>
2022-04-24 08:49:17 +0200

    test

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -21,28 +21,28 @@ export default function Activity() {
     .then(
       (response) => {
         console.log("response.data.id =" + response.data.id);
-        setActivity(response.data)
-        .then(() => {
-          console.log("activity.id dans Activity.js = " + activity.id);
-          return(
-            <Map activity = {activity}/>
-            // Référence, ce qu'on peut afficher (= récupéré dans Activity) : https://developers.strava.com/docs/reference/#api-models-SummaryActivity
-            // <Container fluid className='bg-light border text-black'>
-            //   <Row className="fw-light">Id: {activity.id} </Row>
-            //   <Row className="fw-light">Name: {activity.name} </Row>
-            //   <Row className="fw-light">Moving time: {strTime(activity)} </Row>
-            //   <Row className="fw-light">Elevation gain: {activity.total_elevation_gain}m</Row>
-            //   <Row className="fw-light">Start date: {strDate(activity)}</Row>
-            //   <Row className="fw-light">Average speed: {strSpeed(activity)}</Row>
-            //   <Row className="fw-light">Average cadence: {activity.average_cadence ? activity.average_cadence : "N/A"}</Row>
-            //   <Row className="fw-light">Average heartrate: {activity.average_heartrate ? activity.average_heartrate : "N/A"}</Row>
-            //   {/* <Row className="fw-light">Map:{displayMap(activity)}</Row> */}
-            // </Container>
-          );
-        })
+        setActivity(response.data);
       },
       (error) => { console.log("ERREUR de l'API  : " + error) }
     )
   }
 
+  return(
+    console.log("activity.id dans Activity.js = " + activity.id);
+    return(
+      <Map activity = {activity}/>
+      // Référence, ce qu'on peut afficher (= récupéré dans Activity) : https://developers.strava.com/docs/reference/#api-models-SummaryActivity
+      // <Container fluid className='bg-light border text-black'>
+      //   <Row className="fw-light">Id: {activity.id} </Row>
+      //   <Row className="fw-light">Name: {activity.name} </Row>
+      //   <Row className="fw-light">Moving time: {strTime(activity)} </Row>
+      //   <Row className="fw-light">Elevation gain: {activity.total_elevation_gain}m</Row>
+      //   <Row className="fw-light">Start date: {strDate(activity)}</Row>
+      //   <Row className="fw-light">Average speed: {strSpeed(activity)}</Row>
+      //   <Row className="fw-light">Average cadence: {activity.average_cadence ? activity.average_cadence : "N/A"}</Row>
+      //   <Row className="fw-light">Average heartrate: {activity.average_heartrate ? activity.average_heartrate : "N/A"}</Row>
+      //   {/* <Row className="fw-light">Map:{displayMap(activity)}</Row> */}
+      // </Container>
+    );
+  )
 }
