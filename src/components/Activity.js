7e28a657f6cab6bdcc10551a8df94a284d3e2aaf
florian <florian.carriere@gmail.com>
2022-04-24 16:39:45 +0200

    add info

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -24,32 +24,31 @@ export default function Activity() {
     )
   }
 
+  // Référence, ce qu'on peut afficher (= récupéré dans Activity) : https://developers.strava.com/docs/reference/#api-models-SummaryActivity
+
   if(activity.id) {
     return(
       <div>
         <Map activity = {activity}/>
+        <Container fluid className='bg-light border text-black'>
+          <Row className="fw-light">
+            <Col>Id: {activity.id}</Col>
+            <Col>Name: {activity.name}</Col>
+            <Col>Moving time: {strTime(activity)}</Col>
+            <Col>Elevation gain: {activity.total_elevation_gain}m</Col>
+          </Row>
+          <Row className="fw-light">
+            <Col>Start date: {strDate(activity)}</Col>
+            <Col>Average speed: {strSpeed(activity)}</Col>
+            <Col>Average cadence: {activity.average_cadence ? activity.average_cadence : "N/A"}</Col>
+            <Col>Average heartrate: {activity.average_heartrate ? activity.average_heartrate : "N/A"}</Col>
+          </Row>
+        </Container>
       </div>
-    )
+    );
   } else {
     return(
       <p> Pas d'activité !</p>
     )
   }
-
-  // return(
-  //   <Map activity = {activity}/>
-  //     Référence, ce qu'on peut afficher (= récupéré dans Activity) : https://developers.strava.com/docs/reference/#api-models-SummaryActivity
-  //     <Container fluid className='bg-light border text-black'>
-  //       <Row className="fw-light">Id: {activity.id} </Row>
-  //       <Row className="fw-light">Name: {activity.name} </Row>
-  //       <Row className="fw-light">Moving time: {strTime(activity)} </Row>
-  //       <Row className="fw-light">Elevation gain: {activity.total_elevation_gain}m</Row>
-  //       <Row className="fw-light">Start date: {strDate(activity)}</Row>
-  //       <Row className="fw-light">Average speed: {strSpeed(activity)}</Row>
-  //       <Row className="fw-light">Average cadence: {activity.average_cadence ? activity.average_cadence : "N/A"}</Row>
-  //       <Row className="fw-light">Average heartrate: {activity.average_heartrate ? activity.average_heartrate : "N/A"}</Row>
-  //       {/* <Row className="fw-light">Map:{displayMap(activity)}</Row> */}
-  //     </Container>
-  // );
-
 }
