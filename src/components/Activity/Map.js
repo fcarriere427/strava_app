a8a4604d14f9829df5550c0ba0349b40cba2cb09
florian <florian.carriere@gmail.com>
2022-04-24 15:42:08 +0200

    typo

--- src/components/Activity/Map.js
+++ src/components/Activity/Map.js
@@ -3,21 +3,20 @@ import { Container, Row, Col } from 'reactstrap'
 import { MapContainer, TileLayer, Polyline } from "react-leaflet";
 
 function Map() {
-    return (
-      <MapContainer
-        center={[47.585505245113346, -2.9980409668985826]} //centré sur St Phi ;-)
-        zoom={13}
-        scrollWheelZoom={false}
-        style={{height: '600px'}}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <RunTrace />
-      </MapContainer>
-    );
-  }
+  return (
+    <MapContainer
+      center={[47.585505245113346, -2.9980409668985826]} //centré sur St Phi ;-)
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{height: '600px'}}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <RunTrace />
+    </MapContainer>
+  );
 }
 
 function RunTrace() {
@@ -39,7 +38,6 @@ function RunTrace() {
 
 
   //setView(e.latlng, parentMap.getZoom()
-
 
 }
 
