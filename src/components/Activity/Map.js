0670830643fd43c54dbf8ad35c56e367de5d9062
florian <florian.carriere@gmail.com>
2022-04-24 15:41:25 +0200

    test function components

--- src/components/Activity/Map.js
+++ src/components/Activity/Map.js
@@ -2,22 +2,10 @@ import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap'
 import { MapContainer, TileLayer, Polyline } from "react-leaflet";
 
-class Map extends Component {
-
-  constructor(props){
-    super(props);
-    this.state = { position: "[48, 2]" }; // position: "[48.85928529158136, 2.2934747009596705]"
-  }
-
-  componentDidMount(){
-    this.setState({ position: this.props.activity.start_latlng });
-  }
-
-  render() {
+function Map() {
     return (
       <MapContainer
-        center={this.state.position}
-        //center={[48,2]}
+        center={[47.585505245113346, -2.9980409668985826]} //centré sur St Phi ;-)
         zoom={13}
         scrollWheelZoom={false}
         style={{height: '600px'}}
@@ -26,13 +14,21 @@ class Map extends Component {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {/* <RunTrace /> */}
+        <RunTrace />
       </MapContainer>
     );
   }
 }
 
-class RunTrace extends Component {
+function RunTrace() {
+  return(
+    <p>RunTrace!</p>
+  )
+
+  // const map = useMap();
+  //
+  // this.setState({ position: this.props.activity.start_latlng });
+
 
   //start_position = this.props.activity.start_latlng;
   //let polyline = this.props.activity.map.summary_polyline;
