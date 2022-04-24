6f743cf5feb4bd88bde711af84bc0607261a4145
florian <florian.carriere@gmail.com>
2022-04-24 08:19:24 +0200

    correct

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Switch } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
