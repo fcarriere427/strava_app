ab8ddeab8449b9de706db5b4e0a9925cce73750d
florian <florian.carriere@gmail.com>
2022-04-24 09:05:33 +0200

    typo

--- src/components/Activity/Map.js
+++ src/components/Activity/Map.js
@@ -12,7 +12,7 @@ class Map extends Component {
    }
 
   componentDidMount(){
-    setState({position: this.props.activity.start_latlng };
+    setState({position: this.props.activity.start_latlng });
   }
 
   render() {
