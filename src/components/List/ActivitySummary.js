4199226f1623ab210725c21cd4055f2b60135349
florian <florian.carriere@gmail.com>
2022-04-26 22:29:43 +0200

    test

--- src/components/List/ActivitySummary.js
+++ src/components/List/ActivitySummary.js
@@ -14,23 +14,23 @@ class ActivitySummary extends Component {
      };
    }
 
-   componentDidMount(){
-     let lat = this.props.data.doc.start_latlng[0];
-     let lon = this.props.data.doc.start_latlng[1];
-     let url = 'https://api-adresse.data.gouv.fr/reverse/?lon=' + lon +'&lat=' +lat;
-     axios.get(url,
-       headers: {
-       		'Access-Control-Allow-Origin': '*',
-       		'Content-Type': 'application/json',
-        },
-       	withCredentials: true,
-     )
-     .then(
-       (response) => {
-         this.setState({ country: response.data.features[0].properties.city });
-       },
-       (error) => { console.log("ERREUR de l'API  : " + error) }
-     )
+  componentDidMount(){
+    let lat = this.props.data.doc.start_latlng[0];
+    let lon = this.props.data.doc.start_latlng[1];
+    let url = 'https://api-adresse.data.gouv.fr/reverse/?lon=' + lon +'&lat=' +lat;
+    axios.get(url,{
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
+      withCredentials: true,
+    })
+    .then(
+      (response) => {
+        this.setState({ country: response.data.features[0].properties.city });
+      },
+      (error) => { console.log("ERREUR de l'API  : " + error) }
+    )
   }
 
   render(){
