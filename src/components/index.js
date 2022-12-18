6ac47672083b1de0391cdf25a6605c1ab16fbf28
florian <florian.carriere@gmail.com>
2022-12-18 13:24:26 +0100

    replace home by tracker

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
-export { default as Home } from "./Home";
+//export { default as Home } from "./Home";
+// Remplace la home avec Homer par le tracker
+export { default as Home } from "./Tracker";
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as Tracker } from "./Tracker";
